f87eb82b82fd713701bc14f0b05243d9ba17f574
Thanakorn Thanakraikiti <t.thanakraikiti@gmail.com>
2023-05-13 18:17:33 +0100

    Update TOP LOCATION section to add mean in codes

--- scripts/r/3-data-analysis.R
+++ scripts/r/3-data-analysis.R
@@ -138,6 +138,49 @@ trip_data_v2 %>%
     arrange(member_casual, day_of_week)
 
 
+# ---------------- #
+#   TOP LOCATION   #
+# ---------------- #
+
+# Top 10 starting station throughout 2022
+trip_data_v2 %>%
+    group_by(start_station_name) %>%
+    summarize(
+        ride_count = n(), 
+        mean_ride_length = mean(ride_length)) %>%
+    arrange(desc(ride_count)) %>%
+    top_n(10, ride_count)
+
+# Top 10 ending station throughout 2022
+trip_data_v2 %>%
+    group_by(end_station_name) %>%
+    summarize(
+        ride_count = n(), 
+        mean_ride_length = mean(ride_length)) %>%
+    arrange(desc(ride_count)) %>%
+    top_n(10, ride_count)
+
+# Top 10 starting station for members
+trip_data_v2 %>%
+    group_by(member_casual, start_station_name) %>%
+    filter(member_casual == 'member') %>%
+    summarize(
+        ride_count = n(), 
+        mean_ride_length = mean(ride_length)) %>%
+    arrange(desc(ride_count)) %>%
+    top_n(10, ride_count)
+
+# Top 10 starting station for casual riders
+trip_data_v2 %>%
+    group_by(member_casual, start_station_name) %>%
+    filter(member_casual == 'casual') %>%
+    summarize(
+        ride_count = n(), 
+        mean_ride_length = mean(ride_length)) %>%
+    arrange(desc(ride_count)) %>%
+    top_n(10, ride_count)
+
+
 # -------------------------------------------- #
 #   EXPORT SUMMARY FILE FOR FURTHER ANALYSIS   #
 # -------------------------------------------- #

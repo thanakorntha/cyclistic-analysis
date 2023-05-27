6891a63f6161c7444580465e4f8b70874dc065d5
Thanakorn Thanakraikiti <t.thanakraikiti@gmail.com>
2023-05-27 16:59:24 +0100

    Update comment on two files in data collection
    - 1-data-collection.R
    - 2-data-cleaning.R

--- scripts/r/2-data-cleaning.R
+++ scripts/r/2-data-cleaning.R
@@ -2,21 +2,17 @@
 #   INSPECT PRE-CLEANING DATA FRAME   #
 # ----------------------------------- #
 
-# Get the names of the columns in the 'trip_data' data frame
-# This will allow us to see what data is available in the data frame
+# Get the names of the columns
 colnames(trip_data)
 
-# Get the dimensions of the 'trip_data' data frame
-# This will tell us how many rows and columns there are in the data frame
+# Get the dimensions of the data, telling how many rows and columns there are in the data frame
 dim(trip_data)
 
-# Print the first and last 6 rows of the 'trip_data' data frame
-# This will allow us to see the first and last few rows of data, which will give us a better understanding of the data
+# Print the first and last 6 rows of the data to get a better understanding of the data
 head(trip_data)
 tail(trip_data)
 
-# Print a summary of the 'trip_data' data frame
-# This summary will show us the number of rows and columns in the data, as well as the data types of each column, the minimum and maximum values, and the mean and standard deviation of each column
+# Print a summary of the data, showing the number of rows and columns in the data, as well as the data types of each column, the minimum and maximum values, and the mean and standard deviation of each column
 summary(trip_data)
 
 
@@ -24,17 +20,17 @@ summary(trip_data)
 #   HANDLE DUPLICATE VALUES   #
 # --------------------------- #
 
-# Check if the number of unique ride_id values is equal to the number of rows in trip_data
+# Check if the number of unique values is equal to the number of rows in the 'trip_data' data frame
 if ( length(unique(trip_data$ride_id)) == nrow(trip_data) ) {
-    print("The number of unique ride_id values is equal to the number of rows in trip_data.")
+    print("The number of unique 'ride_id' values is equal to the number of rows in the 'trip_data' table.")
 } else {
-    print("The number of unique ride_id values is not equal to the number of rows in trip_data.")
+    print("The number of unique 'ride_id' values is not equal to the number of rows in the 'trip_data' table.")
 }
 
-# Count the number of rides for each bike type
+# See how many observations fall under each bike type
 count(trip_data, bike_type)
 
-# Count the number of rides for each user type
+# See how many observations fall under each user type
 count(trip_data, user_type)
 
 
@@ -42,20 +38,20 @@ count(trip_data, user_type)
 #   FIX STATION NAME   #
 # -------------------- #
 
-# Create a new data frame called 'trip_data_v2' that is a copy of the 'trip_data' data frame, but with the rows ordered by the 'started_at' column
+# Copy a new data frame but with the rows ordered by 'started_at'
 trip_data_v2 <- trip_data[order(trip_data$started_at), ]
 
-# Remove "start_station_id" and "end_station_id" columns due to not necessity
+# Remove 'start_station_id' and 'end_station_id' due to unnecessary for this analysis
 trip_data_v2 <- trip_data_v2 %>% 
     select(-c(start_station_id, end_station_id))
 
-# Create a data frame called 'start_station' that contains the number of trips that started at each station
+# Examine the starting station names
 start_station <- count(trip_data_v2, start_station_name)
 
-# Create a data frame called 'end_station' that contains the number of trips that ended at each station
+# Examine the ending station names
 end_station <- count(trip_data_v2, end_station_name)
 
-# Remove ride trip data with eight test stations in starting and ending stations
+# Remove all rows that have eight test stations in both the starting and ending locations
 trip_data_v2 <- trip_data_v2[!(trip_data_v2$start_station_name %in% c("Pawel Bialowas - Test- PBSC charging station", 
                                                                       "Hastings WH 2", "DIVVY CASSETTE REPAIR MOBILE STATION", 
                                                                       "Base - 2132 W Hubbard Warehouse", 
@@ -72,7 +68,7 @@ trip_data_v2 <- trip_data_v2[!(trip_data_v2$start_station_name %in% c("Pawel Bia
                                                                       "WestChi", 
                                                                       "WEST CHI-WATSON")), ]
 
-# Remove unwanted characters from the starting and ending station names
+# Remove all unwanted characters from the starting and ending station names
 trip_data_v2 <- trip_data_v2 %>% 
     mutate(start_station_name = str_replace_all(start_station_name, fixed("*"),           "")) %>% 
     mutate(start_station_name = str_replace_all(start_station_name, fixed(" - Charging"), "")) %>% 
@@ -83,56 +79,40 @@ trip_data_v2 <- trip_data_v2 %>%
     mutate(end_station_name   = str_replace_all(end_station_name,   fixed(" (Temp)"),     "")) %>% 
     mutate(end_station_name   = str_replace_all(end_station_name,   fixed("amp;"),        ""))
 
-# Create a data frame called 'start_station_v2' that contains the number of trips that started at each station
-start_station_v2 <- count(trip_data_v2, start_station_name)
-
-# Create a data frame called 'end_station_v2' that contains the number of trips that ended at each station
-end_station_v2 <- count(trip_data_v2, end_station_name)
-
 
 # ------------------------------ #
 #   MANIPLULATE MISSING VALUES   #
 # ------------------------------ #
 
-# Count the total missing number of ride trip data each column in the data frame
-colSums(is.na(trip_data))
+# Count the total missing number each column
+colSums(is.na(trip_data_v2))
 
-# Delete rows with a NA values from the data frame and store in the 'trip_data_v2' variable
-trip_data_v2 <- trip_data[complete.cases(trip_data), ]
+# Remove all rows with eight test stations in starting and ending stations
+# trip_data_v2 <- trip_data_v2[!(is.na(trip_data_v2$end_lat) | is.na(trip_data_v2$end_lng)), ]
 
-# Create a function to find the nearest station
-find_nearest <- function(lat, lng) {
-    # Find the distance between the given point and each station
-    distances <- sqrt((lat - trip_data_v2$start_lat)^2 + (lng - trip_data_v2$start_lng)^2)
-    
-    # Find the index of the station with the shortest distance
-    nearest_index <- which.min(distances)
-    
-    # Return the name and id of the nearest station
-    return(df[nearest_index, "start_station_name"])
-}
+# Fill in the missing values in starting station based on latitude and longitude
+trip_data_v2 <- trip_data_v2 %>% 
+    group_by(start_lat, start_lng) %>% 
+    fill(start_station_name, .direction = "downup") %>% 
+    ungroup()
 
-# Apply the function to find the nearest stations for each row
-trip_data_v2$nearest_start_station_name <- sapply(trip_data_v2$start_lat, trip_data_v2$start_lng, find_nearest)
+# Fill in the missing values in ending station based on latitude and longitude
+trip_data_v2 <- trip_data_v2 %>% 
+    group_by(end_lat, end_lng) %>% 
+    fill(end_station_name, .direction = "downup") %>% 
+    ungroup()
 
-# Repeat the same process for the end stations
-trip_data_v2$nearest_end_station_name <- sapply(trip_data_v2$end_lat, trip_data_v2$end_lng, find_nearest)
+# Let's check the starting station names again
+start_station_v2 <- count(trip_data_v2, start_station_name)
 
-# Write the results to a new file
-write.csv(df, "results.csv")
+# Let's check the ending station names again
+end_station_v2 <- count(trip_data_v2, end_station_name)
 
+# Remove all rows that have any missing values
+trip_data_v2 <- trip_data_v2[complete.cases(trip_data_v2), ]
 
-
-
-
-
-
-
-# Delete rows with a NA values from the data frame and store in the 'trip_data_v2' variable
-trip_data_v2 <- trip_data[complete.cases(trip_data), ]
-
-# See the number of rows and columns of the 'trip_data_v2' data frame
-dim(trip_data_v2)
+# Check to make sure there is no missing values each column
+colSums(is.na(trip_data_v2))
 
 
 # ----------------------------------------------------------------------------- #
@@ -148,50 +128,47 @@ trip_data_v2$day_of_week <- format(as.Date(trip_data_v2$started_at), "%A")
 # Add the 'ride_length' field, displaying in seconds
 trip_data_v2$ride_length <- as.numeric(difftime(trip_data_v2$ended_at, trip_data_v2$started_at, units = "secs"))
 
-# Sort the data frame based on the 'started_at' field in ascending order
-trip_data_v2 <- trip_data_v2[order(trip_data_v2$started_at), ]
 
+# -------------------------- #
+#   REMOVE OUTLIER VALUES    #
+# -------------------------- #
 
-# ------------------------------------ #
-#   REMOVE ERROR AND OUTLIER VALUES    #
-# ------------------------------------ #
-
+# Remove all rows that have ride lengths that are less than 1 minute or greater than 24 hours
 # Remove all ride trip data, where the 'ride_length' field is less than 60 seconds
-trip_data_v2 <- trip_data_v2[!(trip_data_v2$ride_length < 60), ]
+# Remove any ride trip data from the 'trip_data_v2' data frame that have ride lengths that are less than 1 minute or greater than 24 hours
+trip_data_v2 <- trip_data_v2[!(trip_data_v2$ride_length < 60 | trip_data_v2$ride_length > 86400), ]
 
+# Remove outliers
+# 
 # Set values to support removing outliers
-median_value <- median(trip_data_v2$ride_length)  # Define the average of 'ride_length'
-iqr_value    <- IQR(trip_data_v2$ride_length)     # Define the IQR of 'ride_length'
-lower_limit  <- median_value - 1.5 * iqr_value    # Define the limit below Q1
-upper_limit  <- median_value + 1.5 * iqr_value    # Define the limit above Q3
-
+# median_value <- median(trip_data_v2$ride_length)  # Define the average of 'ride_length'
+# iqr_value    <- IQR(trip_data_v2$ride_length)     # Define the IQR of 'ride_length'
+# lower_limit  <- median_value - 1.5 * iqr_value    # Define the limit below Q1
+# upper_limit  <- median_value + 1.5 * iqr_value    # Define the limit above Q3
+# 
 # Print the results
-print(paste("Median:",      median_value))
-print(paste("IQR:",         iqr_value))
-print(paste("Upper Limit:", upper_limit))
-print(paste("Lower Limit:", lower_limit))
-
+# print(paste("Median:",      median_value))
+# print(paste("IQR:",         iqr_value))
+# print(paste("Upper Limit:", upper_limit))
+# print(paste("Lower Limit:", lower_limit))
+# 
 # Remove all ride trip data, where the 'ride_length' field is less than the 'lower_limit' field or more than the 'upper_limit' field in the 'limit' data frame
-trip_data_v2 <- trip_data_v2[!(trip_data_v2$ride_length < lower_limit | trip_data_v2$ride_length > upper_limit), ]
+# trip_data_v2 <- trip_data_v2[!(trip_data_v2$ride_length < lower_limit | trip_data_v2$ride_length > upper_limit), ]
 
 
 # ------------------------------------ #
 #   INSPECT POST-CLEANING DATA FRAME   #
 # ------------------------------------ #
 
-# Get the names of the columns in the 'trip_data_v2' data frame
-# This will allow us to see what data is available in the data frame
+# Get the names of the columns
 colnames(trip_data_v2)
 
-# Get the dimensions of the 'trip_data_v2' data frame
-# This will tell us how many rows and columns there are in the data frame
-dim(trip_data_v2) 
+# Get the dimensions of the data, telling how many rows and columns there are in the data frame
+dim(trip_data_v2)
 
-# Print the first and last 6 rows of the 'trip_data_v2' data frame
-# This will allow us to see the first and last few rows of data, which will give us a better understanding of the data
+# Print the first and last 6 rows of the data to get a better understanding of the data
 head(trip_data_v2)
 tail(trip_data_v2)
 
-# Print a summary of the 'trip_data_v2' data frame
-# This summary will show us the number of rows and columns in the data, as well as the data types of each column, the minimum and maximum values, and the mean and standard deviation of each column
+# Print a summary of the data, showing the number of rows and columns in the data, as well as the data types of each column, the minimum and maximum values, and the mean and standard deviation of each column
 summary(trip_data_v2)
